c62fb91436007a13484ecf7b96eb111c7aa3b5c5
Foram Doshi <63380267+fdoshi@users.noreply.github.com>
2022-11-07 14:37:54 -0600

    practice 11_7

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,14 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const television = {
+  brandName: "LG",
+  color: "Black",
+  screenSize: "40in"
+  status: "Off",
+  changeStatus: function(currentStatus)
+  {
+    this.status = currentStatus;
+  }
+}
